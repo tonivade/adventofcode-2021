d8e06ad58f6028e70ca8515bcd78b7cc0f1877e9
Antonio Muñoz <antonio.munoz@clarity.ai>
2021-12-01 17:45:37 +0100

    day1 part2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -2,11 +2,16 @@ import scala.io.Source
 
 object Day1:
 
-  def solve(input: List[Int]): Int =
+  def part1(input: List[Int]): Int =
     val tuples = input.dropRight(1) zip input.drop(1)
     tuples.count({ case (a, b) => b > a })
+
+  def part2(input: List[Int]): Int =
+    val sums = input.sliding(3).map(_.sum).toList
+    part1(sums)
 
 @main def main: Unit = 
   val numbers = Source.fromFile("input/day1.txt").getLines.map(_.toInt).toList
 
-  println(Day1.solve(numbers))
+  println(Day1.part1(numbers))
+  println(Day1.part2(numbers))
--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -5,5 +5,8 @@ class Day1Test:
 
   val list = List(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
 
-  @Test def t1(): Unit = 
-    assertEquals(7, Day1.solve(list))
+  @Test def part1(): Unit = 
+    assertEquals(7, Day1.part1(list))
+
+  @Test def part2(): Unit = 
+    assertEquals(5, Day1.part2(list))
