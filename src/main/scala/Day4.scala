6912dac4227033b17051de2cd63e665ec4ebce11
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-04 14:35:57 +0100

    Day4 scafolding

---
+++ src/main/scala/Day4.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+import scala.annotation.tailrec
+
+object Day4:
+
+  def part1(input: List[String]): Int = ???
+
+  def part2(input: List[String]): Int = ???
+
+@main def main4: Unit = 
+  val input = Source.fromFile("input/day4.txt").getLines.toList
+
+  println(s"Day4 part1: ${Day4.part1(input)}")
+  println(s"Day4 part2: ${Day4.part2(input)}")
---
+++ src/test/scala/Day4Test.scala
@@ -0,0 +1,8 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+class Day4Test:
+
+  @Test def part1(): Unit = ???
+
+  @Test def part2(): Unit = ???
