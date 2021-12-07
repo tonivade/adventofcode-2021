87d662784c324fd8b0f8962ef7510b087bd85814
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-07 11:44:52 +0100

    Day7 part2

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -3,20 +3,36 @@ import scala.annotation.tailrec
 
 object Day7:
 
-  def distanceTo(position: Int)(crab: Int) =
+  def distanceTo(position: Int)(crab: Int): Int =
     (crab - position).abs
 
-  def part1(input: String): Int = 
-    val crabs = input.split(",").map(_.toInt)
+  def distance2To(position: Int)(crab: Int): Int =
+    distanceTo(position)(crab) match {
+      case 0 => 0
+      case 1 => 1
+      case n => (1 to n).sum
+    }
 
-    val costs = (1 to crabs.size).map { i =>
+  def parse(input: String): Seq[Int] =
+    input.split(",").map(_.toInt)
+
+  def part1(input: String): Int = 
+    val crabs = parse(input)
+
+    val costs = (0 to crabs.size).map { i =>
       crabs.map(distanceTo(i)).sum
     }
 
     costs.min
 
+  def part2(input: String): Int = 
+    val crabs = parse(input)
 
-  def part2(input: String): Int = ???
+    val costs = (0 to crabs.size).map { i =>
+      crabs.map(distance2To(i)).sum
+    }
+
+    costs.min
 
 @main def main7: Unit = 
   val input = Source.fromFile("input/day7.txt").getLines.next
--- src/test/scala/Day7Test.scala
+++ src/test/scala/Day7Test.scala
@@ -9,4 +9,11 @@ class Day7Test:
     assertEquals(37, Day7.part1(input))
 
   @Test def part2(): Unit =
-    assertEquals(26984457539L, Day7.part2(input))
+    assertEquals(168, Day7.part2(input))
+
+  @Test def distance2To(): Unit = {
+    assertEquals(0, Day7.distance2To(1)(1))
+    assertEquals(1, Day7.distance2To(1)(2))
+    assertEquals(3, Day7.distance2To(1)(3))
+    assertEquals(6, Day7.distance2To(1)(4))
+  }
