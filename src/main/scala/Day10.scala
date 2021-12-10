e9917f4997b6b7f19de70460686fdcf4339ab958
Antonio Muñoz <antonio.munoz@clarity.ai>
2021-12-10 10:14:49 +0100

    Day10 part1

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,9 +1,37 @@
 import scala.io.Source
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
 
 object Day10:
 
+  def parseLine(line: String): Option[Char] = 
+    val parse = Try(line.foldLeft(List.empty[Char]) {
+      case (stack, '(') => '(' :: stack
+      case (stack, '[') => '[' :: stack
+      case (stack, '{') => '{' :: stack
+      case (stack, '<') => '<' :: stack
 
-  def part1(input: List[String]): Int = ???
+      case (head :: tail, ')') if (head == '(') => tail
+      case (head :: tail, ']') if (head == '[') => tail
+      case (head :: tail, '}') if (head == '{') => tail
+      case (head :: tail, '>') if (head == '<') => tail
+
+      case (state, c) => throw new IllegalArgumentException(c.toString)
+    })
+    parse match {
+      case Success(_) => None
+      case Failure(e) => Some(e.getMessage()(0))
+    }
+
+  def part1(input: List[String]): Int =
+    val translation = Map(
+      ')' -> 3,
+      ']' -> 57,
+      '}' -> 1197,
+      '>' -> 25137
+    )
+    input.map(parseLine).flatMap(_.toList).map(translation).sum
 
   def part2(input: List[String]): Int = ???
 
--- src/test/scala/Day10Test.scala
+++ src/test/scala/Day10Test.scala
@@ -3,10 +3,19 @@ import org.junit.Assert.*
 
 class Day10Test:
 
-  val input = """""".stripMargin.split("\n").toList
+  val input = """[({(<(())[]>[[{[]{<()<>>
+                |[(()[<>])]({[<{<<[]>>(
+                |{([(<{}[<>[]}>{[]{[(<()>
+                |(((({<>}<{<{<>}{[]{[]{}
+                |[[<[([]))<([[{}[[()]]]
+                |[{[{({}]{}}([{[{{{}}([]
+                |{<[[]]>}<{[{[{[]{()[[[]
+                |[<(<(<(<{}))><([]([]()
+                |<{([([[(<>()){}]>(<<{{
+                |<{([{{}}[<[[[<>{}]]]>[]]""".stripMargin.split("\n").toList
 
   @Test def part1(): Unit = 
-    assertEquals(0, Day10.part1(input))
+    assertEquals(26397, Day10.part1(input))
 
   @Test def part2(): Unit =
     assertEquals(0, Day10.part2(input))
