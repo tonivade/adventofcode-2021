5bb32f79b938b68cf6989711b5c059646f25cfd5
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-11 23:42:25 +0100

    Day11 refactor

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -6,65 +6,79 @@ import scala.annotation.tailrec
 
 object Day11:
 
-  def adjacent(x: Int, y: Int): List[(Int, Int)] =
-    List(
-      (x, y + 1), 
-      (x, y - 1), 
-      (x + 1, y), 
-      (x - 1, y), 
-      (x + 1, y + 1), 
-      (x - 1, y + 1), 
-      (x + 1, y - 1), 
-      (x - 1, y - 1))
+  case class Board(input: List[List[Int]]):
 
-  def step(input: List[List[Int]]): (Int, List[List[Int]]) = 
-    val increment = input.map(_.map(_ + 1))
+    val height = input.size
+    val width = input(0).size
 
-    val all = for {
-      x <- 0 until input(0).size
-      y <- 0 until input.size
+    def all = for {
+      x <- 0 until width
+      y <- 0 until height
     } yield (x, y)
 
-    val after = all.foldLeft((Set.empty[(Int, Int)], increment)) {
-      case ((s, i), (x, y)) => flash(x, y)(s, i)
-    }
+    def adjacent(x: Int, y: Int): List[(Int, Int)] =
+      List(
+        (x, y + 1), 
+        (x, y - 1), 
+        (x + 1, y), 
+        (x - 1, y), 
+        (x + 1, y + 1), 
+        (x - 1, y + 1), 
+        (x + 1, y - 1), 
+        (x - 1, y - 1)).filter {
+          (x, y) => x > -1 && y > -1 && x < input(0).size && y < input.size
+        }
+    
+    def increment = Board(input.map(_.map(_ + 1)))
 
-    (after._1.size, after._2.map(_.map(x => if (x > 9) 0 else x)))
+    def get(x: Int, y: Int) = input(y)(x)
+
+    def update(x: Int, y: Int): Board = 
+      Board(input.updated(y, input(y).updated(x, get(x, y) + 1)))
+
+    def clear = Board(input.map(_.map(x => if (x > 9) 0 else x)))
+
+    def allFlash = input.flatMap(identity).sum == 0
+
+    def step: (Int, Board) = 
+      val (state, output) = all.foldLeft((Set.empty[(Int, Int)], increment)) {
+        case ((s, i), (x, y)) => flash(x, y)(s, i)
+      }
+
+      (state.size, output.clear)
   
-  def flash(x: Int, y: Int)(state: Set[(Int, Int)], input: List[List[Int]]): (Set[(Int, Int)], List[List[Int]]) =
-    if (!state.contains(x, y) && input(y)(x) > 9)
-      val adj = adjacent(x, y).filter {
-        (x, y) => x > -1 && y > -1 && x < input(0).size && y < input.size
-      }
-      adj.foldLeft((state + ((x, y)), input)) {
-        case ((s, i), (x1, y1)) => flash(x1, y1)(s, i.updated(y1, i(y1).updated(x1, i(y1)(x1) + 1)))
-      }
-    else 
-      (state, input)
+    def flash(x: Int, y: Int)(state: Set[(Int, Int)], input: Board): (Set[(Int, Int)], Board) =
+      if (!state.contains(x, y) && input.get(x, y) > 9)
+        val adj = input.adjacent(x, y)
+        adj.foldLeft((state + ((x, y)), input)) {
+          case ((s, i), (x1, y1)) => flash(x1, y1)(s, i.update(x1, y1))
+        }
+      else 
+        (state, input)
 
   @tailrec
-  def play(n: Int, count: Int, input: List[List[Int]]): (Int, List[List[Int]]) =
+  def play(n: Int, count: Int, input: Board): (Int, Board) =
     if (n > 0)
-      val (x, next) = step(input)
+      val (x, next) = input.step
       play(n - 1, count + x, next)
     else
       (count, input)
 
   @tailrec
-  def sync(n: Int, input: List[List[Int]]): Int =
-    if (input.flatMap(identity).sum != 0)
-      val (_, next) = step(input)
+  def sync(n: Int, input: Board): Int =
+    if (!input.allFlash)
+      val (_, next) = input.step
       sync(n + 1, next)
     else
       n
 
   def part1(input: List[String]): Int =
     val parsed = input.map(_.map(_.toString.toInt).toList)
-    play(100, 0, parsed)._1
+    play(100, 0, Board(parsed))._1
 
   def part2(input: List[String]): Int =
     val parsed = input.map(_.map(_.toString.toInt).toList)
-    sync(0, parsed)
+    sync(0, Board(parsed))
 
 @main def main11: Unit = 
   val input = Source.fromFile("input/day11.txt").getLines.toList
--- src/test/scala/Day11Test.scala
+++ src/test/scala/Day11Test.scala
@@ -18,32 +18,32 @@ class Day11Test:
     assertEquals(1656, Day11.part1(input))
 
   @Test def part1step(): Unit = 
-    val start = List(
+    val start = Day11.Board(List(
       List(1, 1, 1, 1, 1),
       List(1, 9, 9, 9, 1),
       List(1, 9, 1, 9, 1),
       List(1, 9, 9, 9, 1),
       List(1, 1, 1, 1, 1),
-    )
+    ))
 
-    val step1 = List(
+    val step1 = Day11.Board(List(
       List(3, 4, 5, 4, 3),
       List(4, 0, 0, 0, 4),
       List(5, 0, 0, 0, 5),
       List(4, 0, 0, 0, 4),
       List(3, 4, 5, 4, 3)
-    )
+    ))
 
-    val step2 = List(
+    val step2 = Day11.Board(List(
       List(4, 5, 6, 5, 4),
       List(5, 1, 1, 1, 5),
       List(6, 1, 1, 1, 6),
       List(5, 1, 1, 1, 5),
       List(4, 5, 6, 5, 4)
-    )
+    ))
 
-    assertEquals((9, step1), Day11.step(start))
-    assertEquals((0, step2), Day11.step(step1))
+    assertEquals((9, step1), start.step)
+    assertEquals((0, step2), step1.step)
 
   @Test def part2(): Unit =
     assertEquals(195, Day11.part2(input))
