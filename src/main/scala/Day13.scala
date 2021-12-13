084d42aad9d2dd4ddee7e85056adb1b54bf09a1b
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-14 00:01:47 +0100

    day13 part1

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -2,12 +2,68 @@ import scala.io.Source
 
 object Day13:
 
-  def part1(input: List[String]): Int = ???
+  sealed trait Fold
+  case class FoldX(x: Int) extends Fold
+  case class FoldY(y: Int) extends Fold
 
-  def part2(input: List[String]): Int = ???
+  def parseDots(input: String): List[(Int, Int)] = 
+    input.split("\n").map(_.split(",")).map {
+      case Array(x, y) => (x.toInt, y.toInt)
+    }.toList
+
+  def parseFolds(input: String): List[Fold] = 
+    val foldx = """fold along x=(\d+)""".r
+    val foldy = """fold along y=(\d+)""".r
+    input.split("\n").map {
+      case foldx(value) => FoldX(value.toInt)
+      case foldy(value) => FoldY(value.toInt)
+    }.toList
+
+  def printDots(dots: List[(Int, Int)]): List[List[Int]] =
+    val maxX = dots.map(_._1).max + 1
+    val maxY = dots.map(_._2).max + 1
+    dots.foldLeft(List.fill(maxY, maxX)(0)) {
+      case (matrix, (x, y)) => matrix.updated(y, matrix(y).updated(x, 1))
+    }
+
+  def mergeY(top: List[List[Int]], bottom: List[List[Int]]): List[List[Int]] =
+    val reverse = bottom.reverse
+    top.zipWithIndex.map {
+      (line, y) => line.zipWithIndex.map {
+        (value, x) => value + reverse(y)(x)
+      }
+    }
+  
+  def mergeX(left: List[List[Int]], right: List[List[Int]]): List[List[Int]] = 
+    val reverse = right.map(_.reverse)
+    left.zipWithIndex.map {
+      (line, y) => line.zipWithIndex.map {
+        (value, x) => value + reverse(y)(x)
+      }
+    }
+  
+  def applyFirstFold(map: List[List[Int]], fold: Fold): List[List[Int]] =
+    fold match {
+      case FoldX(x) => 
+        val s = map.map(_.splitAt(x))
+        mergeX(s.map(_._1), s.map(_._2.tail))
+      case FoldY(y) => 
+        val s = map.splitAt(y)
+        mergeY(s._1, s._2.tail)
+    }
+
+  def part1(input: String): Int = 
+    val (dots, folds) = input.split("\n\n") match {
+      case Array(d, f) => (parseDots(d), parseFolds(f))
+    }
+    val map = printDots(dots)
+    applyFirstFold(map, folds(0)).flatMap(identity).count(_ > 0)
+
+  def part2(input: String): Int = ???
 
 @main def main13: Unit = 
-  val input = Source.fromFile("input/day13.txt").getLines.toList
+  val input = Source.fromFile("input/day13.txt").mkString
 
   println(s"Day13 part1: ${Day13.part1(input)}")
   println(s"Day13 part2: ${Day13.part2(input)}")
+  
--- src/test/scala/Day13Test.scala
+++ src/test/scala/Day13Test.scala
@@ -4,10 +4,30 @@ import org.junit.Ignore
 
 class Day13Test:
 
-  val input = """""".stripMargin.split("\n").toList
+  val input = """6,10
+                |0,14
+                |9,10
+                |0,3
+                |10,4
+                |4,11
+                |6,0
+                |6,12
+                |4,1
+                |0,13
+                |10,12
+                |3,4
+                |3,0
+                |8,4
+                |1,10
+                |2,14
+                |8,10
+                |9,0
+                |
+                |fold along y=7
+                |fold along x=5""".stripMargin
 
   @Test def part1(): Unit = 
-    assertEquals(0, Day13.part1(input))
+    assertEquals(17, Day13.part1(input))
 
   @Test def part2(): Unit = 
     assertEquals(0, Day13.part2(input))
