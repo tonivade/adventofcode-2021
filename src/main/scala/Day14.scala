b0cf1ec27ba882eecf6519f39b92752839b9f72c
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-14 22:37:46 +0100

    Day14 part1

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -2,7 +2,37 @@ import scala.io.Source
 
 object Day14:
 
-  def part1(input: String): Int = ???
+  def parseMappings(input: List[String]): Map[String, String] = 
+    val mappingregex = """(\w\w) -> (\w)""".r
+    input.map {
+      case mappingregex(left, right) => (left, right)
+    }.toMap
+
+  def step(seed: String, mapping: Map[String, String]): String = 
+    val seq = seed.dropRight(1) zip seed.tail
+    seq.map {
+      case (a, b) => a.toString + mapping(Array(a, b).mkString)
+    }.mkString + seed.last
+
+  def go(n: Int, seed: String, mapping: Map[String, String]): String =
+    if (n > 0)
+      val next = step(seed, mapping)
+      go(n - 1, next, mapping)
+    else
+      seed
+
+  def parse(input: String): (String, Map[String, String]) =
+    input.split("\n\n") match {
+      case Array(s, m) => (s, parseMappings(m.split("\n").toList))
+    }
+
+  def part1(input: String): Int = 
+    val (seed, mapping) = parse(input)
+    val polymer = go(10, seed, mapping)
+    val grouped = polymer.groupBy(identity).mapValues(_.size).toMap
+    val max = grouped.maxBy(_._2)._2
+    val min = grouped.minBy(_._2)._2
+    max - min
 
   def part2(input: String): Int = ???
 
--- src/test/scala/Day14Test.scala
+++ src/test/scala/Day14Test.scala
@@ -23,7 +23,15 @@ class Day14Test:
                 |CN -> C""".stripMargin
 
   @Test def part1(): Unit = 
-    assertEquals(0, Day14.part1(input))
+    assertEquals(1588, Day14.part1(input))
+
+  @Test def step1(): Unit = 
+    val mapping = Map("NN" -> "C", "NC" -> "B", "CB" -> "H")
+    assertEquals("NCNBCHB", Day14.step("NNCB", mapping))
+
+  @Test def step4(): Unit = 
+    val (seed, mapping) = Day14.parse(input)
+    assertEquals("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", Day14.go(4, seed, mapping))
 
   @Test def part2(): Unit = 
     assertEquals(0, Day14.part2(input))
