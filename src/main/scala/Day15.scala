a7d6b1d678d8666854e7dd941da3aae5e1e082f9
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-15 22:09:58 +0100

    Day15 scafolding

---
+++ src/main/scala/Day15.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+
+object Day15:
+
+  def part1(input: List[String]): Int = ???
+
+  def part2(input: List[String]): Int = ???
+
+@main def main15: Unit = 
+  val input = Source.fromFile("input/day15.txt").getLines.toList
+
+  println(s"Day15 part1: ${Day15.part1(input)}")
+  println(s"Day15 part2: ${Day15.part2(input)}")
+  
---
+++ src/test/scala/Day15Test.scala
@@ -0,0 +1,21 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+class Day15Test:
+
+  val input = """1163751742
+                |1381373672
+                |2136511328
+                |3694931569
+                |7463417111
+                |1319128137
+                |1359912421
+                |3125421639
+                |1293138521
+                |2311944581""".stripMargin.split("\n").toList
+
+  @Test def part1(): Unit = 
+    assertEquals(0, Day15.part1(input))
+
+  @Test def part2(): Unit = 
+    assertEquals(0, Day15.part2(input))
