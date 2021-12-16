3e99b9336a00150cd578dedd86428cb382945893
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-17 00:04:12 +0100

    Day16 part1 WIP

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -1,16 +1,104 @@
 import scala.io.Source
-import java.util.PriorityQueue
-import scala.collection.mutable
 import scala.annotation.tailrec
 
 object Day16:
 
-  def part1(input: List[String]): Int = ???
+  def binaryToInt(binary: String): Int = 
+    binary.reverse.zipWithIndex.filter((x, _) => x == '1').map((_, i) => 1 << i).sum
 
-  def part2(input: List[String]): Int = ???
+  sealed trait Packet:
+    def version: Int
+
+  case class Literal(version: Int, value: Int) extends Packet
+
+  case class Operator(version: Int, operands: List[Packet]) extends Packet
+
+  val translation: Map[Char, String] = Map(
+    '0' -> "0000",
+    '1' -> "0001",
+    '2' -> "0010",
+    '3' -> "0011",
+    '4' -> "0100",
+    '5' -> "0101",
+    '6' -> "0110",
+    '7' -> "0111",
+    '8' -> "1000",
+    '9' -> "1001",
+    'A' -> "1010",
+    'B' -> "1011",
+    'C' -> "1100",
+    'D' -> "1101",
+    'E' -> "1110",
+    'F' -> "1111"
+  )
+
+  def parse(input: String): Packet =
+    val binary = input.flatMap(translation)
+    val (packet, _) = parseOne(binary)
+    packet
+
+  def parseOne(binary: String): (Packet, Int) = 
+    val version = binaryToInt(binary.take(3))
+    val typeId = binaryToInt(binary.drop(3).take(3))
+
+    typeId match {
+      // literal
+      case 4 => 
+        def go(input: String): String =
+          val current = input.take(5)
+          if (current.charAt(0) == '1')
+            current.drop(1) + go(input.drop(5))
+          else
+            current.drop(1)
+
+        val value = go(binary.drop(3 + 3))
+        val consumed = 3 + 3 + value.size + (value.size / 4)
+        (Literal(version, binaryToInt(value)), consumed)
+      // operator
+      case _ => 
+        binary.drop(3 + 3).charAt(0) match {
+          // bits
+          case '0' => 
+            val bitLength = binaryToInt(binary.drop(3 + 3 + 1).take(15))
+            val bits = binary.drop(3 + 3 + 1 + 15).take(bitLength)
+            val (packets, consumed) = parseMany(bits)
+            println(s"bits ${bitLength} == ${consumed}")
+            assert(bitLength == consumed)
+            (Operator(version, packets), 3 + 3 + 1 + 15 + bitLength)
+          // packets
+          case '1' => 
+            val packetLength = binaryToInt(binary.drop(3 + 3 + 1).take(11))
+            val bits = binary.drop(3 + 3 + 1 + 11)
+            val (packets, consumed) = parseMany(bits)
+            println(s"packets ${packets.size} == ${packetLength}")
+            if (packets.size != packetLength)
+              println(binary)
+            assert(packets.size == packetLength)
+            (Operator(version, packets), 3 + 3 + 1 + 11 + consumed)
+        }
+    }
+
+  def parseMany(binary: String): (List[Packet], Int) = 
+    if (binary.length < 6)
+      (Nil, 0)
+    else
+      val (head, consumed) = parseOne(binary)
+      val (tail, consumedAll) = parseMany(binary.drop(consumed))
+      (head :: tail, consumed + consumedAll)
+
+  def part1(input: String): Int =
+    val packet = parse(input)
+    def count(packet: Packet): Int =
+      packet match {
+        case Literal(version, value) => version
+        case Operator(version, packets) => version + packets.map(count).sum
+      }
+    count(packet)
+
+  def part2(input: String): Int = ???
 
 @main def main16: Unit = 
-  val input = Source.fromFile("input/day16.txt").getLines.toList
+  val input = Source.fromFile("input/day16.txt").getLines.next
 
   println(s"Day16 part1: ${Day16.part1(input)}")
   println(s"Day16 part2: ${Day16.part2(input)}")
--- src/test/scala/Day16Test.scala
+++ src/test/scala/Day16Test.scala
@@ -1,12 +1,32 @@
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Ignore
 
 class Day16Test:
+  import Day16.{ Literal, Operator, parse }
 
-  val input = """""".stripMargin.split("\n").toList
+  val input = ""
 
-  @Test def part1(): Unit = 
-    assertEquals(0, Day16.part1(input))
+  @Test def parse1(): Unit =
+    assertEquals(Literal(6, 2021), parse("D2FE28"))
 
-  @Test def part2(): Unit = 
+  @Test def parse2(): Unit =
+    assertEquals(Operator(1, List(Literal(6, 10), Literal(2, 20))), parse("38006F45291200"))
+
+  @Test def part10(): Unit = 
+    assertEquals(9, Day16.part1("38006F45291200"))
+
+  @Test def part11(): Unit = 
+    assertEquals(16, Day16.part1("8A004A801A8002F478"))
+
+  @Test def part12(): Unit = 
+    assertEquals(12, Day16.part1("620080001611562C8802118E34"))
+
+  @Test def part13(): Unit = 
+    assertEquals(23, Day16.part1("C0015000016115A2E0802F182340"))
+
+  @Test def part14(): Unit = 
+    assertEquals(31, Day16.part1("A0016C880162017C3686B18A3D4780"))
+
+  @Test @Ignore def part2(): Unit = 
     assertEquals(0, Day16.part2(input))
