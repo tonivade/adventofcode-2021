a39beb49eedcb162c931c5c4f49fbcdc2cba8998
Antonio Muñoz <antonio.munoz@clarity.ai>
2021-12-16 21:42:24 +0100

    Day16 scafolding

---
+++ src/main/scala/Day16.scala
@@ -0,0 +1,17 @@
+import scala.io.Source
+import java.util.PriorityQueue
+import scala.collection.mutable
+import scala.annotation.tailrec
+
+object Day16:
+
+  def part1(input: List[String]): Int = ???
+
+  def part2(input: List[String]): Int = ???
+
+@main def main16: Unit = 
+  val input = Source.fromFile("input/day16.txt").getLines.toList
+
+  println(s"Day16 part1: ${Day16.part1(input)}")
+  println(s"Day16 part2: ${Day16.part2(input)}")
+  
---
+++ src/test/scala/Day16Test.scala
@@ -0,0 +1,12 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+class Day16Test:
+
+  val input = """""".stripMargin.split("\n").toList
+
+  @Test def part1(): Unit = 
+    assertEquals(0, Day16.part1(input))
+
+  @Test def part2(): Unit = 
+    assertEquals(0, Day16.part2(input))
