79a06daf12154f4e32334ab733815177117bfcd9
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-18 22:39:22 +0100

    Day17 part1

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,8 +1,57 @@
 import scala.io.Source
+import scala.annotation.tailrec
 
 object Day17:
 
-  def part1(input: String): Int = ???
+  type Position = (Int, Int)
+  type Speed = (Int, Int)
+  
+  case class Target(x1: Int, x2: Int, y1: Int, y2: Int):
+    def contains(probe: Probe): Boolean =
+      probe.position match {
+        case (x, y) => x >= x1 && x <= x2 && y >= y1 && y <= y2
+      }
+    def missed(probe: Probe): Boolean = 
+      probe.position match {
+        case (x, y) => x > x2 || y < y2
+      }
+
+  case class Probe(position: Position, speed: Speed, path: List[(Int, Int)] = Nil):
+    def maxY: Int = path.map(_._2).max
+
+  def step(probe: Probe): Probe =
+    probe match {
+      case Probe((px, py), (sx, sy), path) if (sx > 0) => 
+        Probe((px + sx, py + sy), (sx - 1, sy - 1), (px, py) :: path)
+      case Probe((px, py), (sx, sy), path) if (sx < 0) => 
+        Probe((px + sx, py + sy), (sx + 1, sy - 1), (px, py) :: path)
+      case Probe((px, py), (sx, sy), path) if (sx == 0) => 
+        Probe((px, py + sy), (0, sy - 1), (px, py) :: path)
+    }
+
+  @tailrec
+  def launch(target: Target)(probe: Probe): Option[Probe] =
+    if (target.contains(probe))
+      Some(probe)
+    else if (target.missed(probe))
+      None
+    else
+      val nextProbe = step(probe)
+      launch(target)(nextProbe)
+
+  def part1(input: String): Int = 
+    val targetregex = """target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)""".r
+    val target = input match {
+      case targetregex(x1, x2, y1, y2) => Target(x1.toInt, x2.toInt, y1.toInt, y2.toInt)
+    }
+    val all = for {
+      x <- 0 to target.x2
+      y <- 0 to target.y1.abs
+    } yield (x, y)
+
+    val result = all.map(Probe((0, 0), _)).map(launch(target)).flatMap(_.toList)
+
+    result.map(_.maxY).max
 
   def part2(input: String): Int = ???
 
--- src/test/scala/Day17Test.scala
+++ src/test/scala/Day17Test.scala
@@ -1,12 +1,44 @@
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Ignore
 
 class Day17Test:
+  import Day17.{Target, Probe, launch }
 
   val input: String = "target area: x=20..30, y=-10..-5"
 
-  @Test def part1(): Unit = 
-    assertEquals(0, Day17.part1(input))
+  val initial = (0, 0)
+  val target = Target(20, 30, -10, -5)
 
-  @Test def part20(): Unit = 
+  @Test def target1(): Unit =
+    assertEquals(
+      Some(Probe((28,-7), (0,-5), List((27,-3), (25,0), (22,2), (18,3), (13,3), (7,2), (0,0)))), 
+      launch(target)(Probe(initial, (7, 2))))
+
+  @Test def target2(): Unit =
+    assertEquals(
+      Some(Probe((21,-9), (0,-6), List((21,-4), (21,0), (21,3), (20,5), (18,6), (15,6), (11,5), (6,3), (0,0)))), 
+      launch(target)(Probe(initial, (6, 3))))
+
+  @Test def target3(): Unit =
+    assertEquals(
+      Some(Probe((30,-6), (5,-4), List((24,-3), (17,-1), (9,0), (0,0)))), 
+      launch(target)(Probe(initial, (9, 0))))
+
+  @Test def target4(): Unit =
+    val result = launch(target)(Probe(initial, (6, 9)))
+    assertEquals(
+      Some(Probe((21,-10), (0,-11), List((21,0), (21,9), (21,17), (21,24), (21,30), (21,35), (21,39), (21,42), (21,44), (21,45), (21,45), (21,44), (21,42), (21,39), (20,35), (18,30), (15,24), (11,17), (6,9), (0,0)))), 
+      result)
+    assertEquals(45, result.get.maxY)
+
+  @Test def target5(): Unit =
+    assertEquals(
+      None, 
+      launch(target)(Probe(initial, (17, -4))))
+
+  @Test def part1(): Unit = 
+    assertEquals(45, Day17.part1(input))
+
+  @Test @Ignore def part2(): Unit = 
     assertEquals(0, Day17.part2(input))
