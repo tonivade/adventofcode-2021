f10b56568efe0c96eaac84b34f3bcdaeb09295f6
Antonio Muñoz <antoniogmc@gmail.com>
2021-12-19 02:23:15 +0100

    Day18 part1 WIP

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -3,9 +3,36 @@ import scala.annotation.tailrec
 
 object Day18:
 
-  sealed trait Node
+  sealed trait Node:
+
+    def add(other: Node): Node = Pair(this, other)
+
+    def split(toSplit: Leaf): Node = 
+      this match {
+        case Leaf(value) if (this eq toSplit) => 
+          val l = value / 2
+          val r = (value / 2) + (value % 2)
+          Pair(Leaf(l), Leaf(r))
+        case Pair(left, right) => Pair(left.split(toSplit), right.split(toSplit))
+        case _ => this
+      }
+
+    def explode(toExplode: Pair): Node =
+      this match {
+        case Pair(left, right) if (left eq toExplode) => ???
+        case Pair(left, right) if (right eq toExplode) => ???
+        case Pair(left, right) => Pair(left.explode(toExplode), right.explode(toExplode))
+        case _ => this
+      }
+
   case class Leaf(value: Int) extends Node
   case class Pair(left: Node, right: Node) extends Node
+
+  sealed trait Action
+  case class Split(leaf: Leaf) extends Action
+  case class Explode(pair: Pair) extends Action
+
+  def parse(input: String): Node = parseNode(input)._1
 
   def parseNode(input: String): (Node, String) =
     input.head match {
@@ -18,7 +45,27 @@ object Day18:
     val (right, second) = parseNode(first.tail)
     (Pair(left, right), second.tail)
 
-  def parse(input: String): Node = parseNode(input)._1
+  def toSplit(node: Node): Option[Action] = 
+    node match {
+      case Leaf(value) if (value > 9) => Some(Split(node.asInstanceOf[Leaf]))
+      case Pair(left, right) => toSplit(left) orElse toSplit(right)
+      case _ => None
+    }
+
+  def toExplode(node: Node, depth: Int = 0): Option[Action] = 
+    node match {
+      case Pair(left, right) if (depth == 4) => Some(Explode(node.asInstanceOf[Pair]))
+      case Pair(left, right) => toExplode(left, depth + 1) orElse toExplode(right, depth + 1)
+      case _ => None
+    }
+
+  def reduce(node: Node): Node =
+    val action = toExplode(node) orElse toSplit(node)
+    action match {
+      case Some(Split(leaf)) => node.split(leaf)
+      case Some(Explode(pair)) => node.explode(pair)
+      case None => node
+    }
 
   def part1(input: String): Int = ???
 
--- src/test/scala/Day18Test.scala
+++ src/test/scala/Day18Test.scala
@@ -7,9 +7,16 @@ class Day18Test:
 
   val input: String = """"""
 
-  @Test def parse1(): Unit = 
+  @Test def parse(): Unit = 
     assertEquals(Pair(Leaf(1), Leaf(2)), Day18.parse("[1,2]"))
     assertEquals(Pair(Pair(Leaf(1), Leaf(2)), Leaf(3)), Day18.parse("[[1,2],3]"))
+  
+  @Test def add(): Unit =
+    assertEquals(Pair(Pair(Leaf(1), Leaf(2)), Pair(Leaf(3), Leaf(4))), Pair(Leaf(1), Leaf(2)) add Pair(Leaf(3), Leaf(4)))
+
+  @Test def split(): Unit =
+    assertEquals(Pair(Pair(Leaf(5), Leaf(5)), Leaf(1)), Day18.reduce(Pair(Leaf(10), Leaf(1)))) 
+    assertEquals(Pair(Pair(Leaf(5), Leaf(6)), Leaf(1)), Day18.reduce(Pair(Leaf(11), Leaf(1)))) 
 
   @Test @Ignore def part1(): Unit = 
     assertEquals(0, Day18.part1(input))
